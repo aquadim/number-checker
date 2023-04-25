8893e274282674de437e73f640343e06856647c5
aquadim <87524324+aquadim@users.noreply.github.com>
2023-04-25 21:54:15 +0300

    Добавлены комментарии
    добавлены через интерфейс gihub, т.к. не сохранились комментарии из pull request-а

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ namespace number_checker
 {
     internal class Program
     {
+        // Функция вычисления ответа
         static int Calculate(int[] Array, int n)
-        { int output = 0;
-          foreach(int m in Array)
+        { int output = 0; // Сколько раз n встречается в Array
+          foreach(int m in Array) // Сверяем все элементы с n
             {
                 if(m == n) 
                 {
+                    // Элемент равен n
                     output++;
                 }
             }
